7be2f92da899e8c0831c364bcff7588d4dcc00da
Bartekwis01 <Bartekwis01@gmail.com>
2026-01-12 16:59:20 +0100

    Re-add a way to run the program standalone

---
+++ standalone.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "typoglycemia.h"
+
+int main() {
+    std::string text;
+    getline(std::cin, text); //gets whole line instead of only 1 word to space
+    std:: string typoglycemized_text = typoglycemize(text, true, true, true);
+    std::cout << typoglycemized_text << std::endl;
+}
