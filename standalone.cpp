93974b621e0cbaef15650b76e9a464dcf7c4cf1b
Bartekwis01 <Bartekwis01@gmail.com>
2026-01-12 17:26:55 +0100

    Add Typoglycemia.exe standalone program

--- standalone.cpp
+++ standalone.cpp
@@ -6,4 +6,6 @@ int main() {
     getline(std::cin, text); //gets whole line instead of only 1 word to space
     std:: string typoglycemized_text = typoglycemize(text, true, true, true);
     std::cout << typoglycemized_text << std::endl;
+    std::cout << "Press any key to close";
+    std::cin.get();
 }
